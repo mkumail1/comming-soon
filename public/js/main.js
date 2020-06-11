64c289bda44d954d5f9fb0c692edd49b208c75fb
Muhammad Kumail <mkumailofficial@gmail.com>
2020-06-12 04:07:43 +0500

    responsive v1

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-(function() {
     'use strict';
     window.addEventListener('load', function() {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -44,6 +43,17 @@
 
     $('.button').on('click', function () {
         $(this).blur()
-    })
-})();
+    });
+
+    function myFunction(x) {
+        if (x.matches) { // If media query matches
+            $('.description').html('Rexia is a product-based Pakistani self grown IT company aiming to produce future leaders of the world!');
+        } else {
+            $('.description').html('Rexia is a product-based Pakistani self grown IT company aiming </br> to produce future leaders of the world!');
+        }
+      }
+      
+      var x = window.matchMedia("(max-width: 1328px)")
+      myFunction(x) // Call listener function at run time
+      x.addListener(myFunction) // Attach listener function on state changes
 
